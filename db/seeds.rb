625fdf57864eff1d3250aaff4b250cfe1d5fc5b7
NaingThuAung25 <naingthu20317@gmail.com>
2024-02-08 18:04:33 +0700

    Adding user

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+User.create(email: 'yeemon@gmail.com', password: 'password', password_confirmation: 'password')
+
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here")
+    Post.create(title: "Title #{x}", body: "Body #{x} Words go here", user_id: User.first.id)
 end
